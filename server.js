7e904941d09e795ae8a62c29805fd1c04f3e3f64
azuh salvation <salvationazuh@gmail.com>
2026-04-29 15:43:38 +0100

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://insighta-web-vert.vercel.app",
     credentials: true,
   }),
 );
