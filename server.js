b7e531aa00c8d7f223d67b9d3c3b727859116150
azuh salvation <salvationazuh@gmail.com>
2026-04-29 14:53:13 +0100

    fix(cors): allow credentials from localhost:3000

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000",
+    credentials: true,
   }),
 );
 app.use(express.json());
